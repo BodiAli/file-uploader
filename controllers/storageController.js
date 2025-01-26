73cce19241a5565bd7b1fc6d97306b8f1e66035c
Bodi Ali <bodiali59@gmail.com>
2025-01-26 19:56:13 +0200

    Handle getting all folders and formatting the dates and handle creating folder

--- controllers/storageController.js
+++ controllers/storageController.js
@@ -1,9 +1,38 @@
 const asyncHandler = require("express-async-handler");
+const { formatDistanceToNow } = require("date-fns");
 const { isAuthenticated } = require("./authenticationController");
+const prisma = require("../prisma/prismaClient");
 
 exports.getStoragePage = [
   isAuthenticated,
-  asyncHandler((req, res) => {
-    res.render("storage");
+  asyncHandler(async (req, res) => {
+    const result = await prisma.folder.findMany({
+      where: {
+        userId: req.user.id,
+      },
+    });
+
+    const folders = result.map((value) => ({
+      ...value,
+      createdAt: formatDistanceToNow(value.createdAt, { addSuffix: true, includeSeconds: true }),
+      updatedAt: formatDistanceToNow(value.updatedAt, { addSuffix: true, includeSeconds: true }),
+    }));
+
+    res.render("storage", { folders });
+  }),
+];
+
+exports.createFolder = [
+  asyncHandler(async (req, res) => {
+    await prisma.folder.create({
+      data: {
+        name: req.body.folderName,
+        userId: req.user.id,
+      },
+    });
+
+    const referrer = req.header("referrer") || "/storage";
+
+    res.redirect(referrer);
   }),
 ];
