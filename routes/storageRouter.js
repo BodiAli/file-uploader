5ba92cda2ec93372247bb79c210e78c314c9368a
Bodi Ali <bodiali59@gmail.com>
2025-01-27 22:12:48 +0200

    Handle post requests to delete folder and requests to getting folder

--- routes/storageRouter.js
+++ routes/storageRouter.js
@@ -4,9 +4,11 @@ const storageController = require("../controllers/storageController");
 const storageRouter = Router();
 
 storageRouter.get("/", storageController.getStoragePage);
+storageRouter.get("/folder/:id", storageController.getFolderPage);
 
 storageRouter.post("/folder/create", storageController.createFolder);
 
-// storageRouter.post("/folder/:id/edit", storageController.updateFolder);
+storageRouter.post("/folder/:id/edit", storageController.updateFolder);
+storageRouter.post("/folder/:id/delete", storageController.deleteFolder);
 
 module.exports = storageRouter;
