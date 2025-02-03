09eb0dc5b97ef9be2722f1aa5bac4ca5e82b4cf1
Bodi Ali <bodiali59@gmail.com>
2025-02-04 00:02:09 +0200

    Handle request to update files && Add minor changes

--- routes/storageRouter.js
+++ routes/storageRouter.js
@@ -5,15 +5,19 @@ const storageRouter = Router();
 
 storageRouter.get("/", storageController.getStoragePage);
 storageRouter.get("/folder/:id", storageController.getFolderPage);
+
 storageRouter.get("/file/:id", storageController.getFilePage);
 
 storageRouter.post("/folder/create", storageController.createFolder);
 storageRouter.post("/folder/create/:id", storageController.createFolder);
 
+storageRouter.post("/file/create/:id", storageController.createFile);
+
 storageRouter.post("/folder/:id/edit", storageController.updateFolder);
 storageRouter.post("/folder/:id/delete", storageController.deleteFolder);
 
-storageRouter.post("/file/create/:id", storageController.createFile);
+storageRouter.post("/file/:id/edit", storageController.updateFile);
+// storageRouter.post("/file/create/:id", storageController.createFile);
 
 storageRouter.post("/file/:id/download", storageController.downloadFile);
 
