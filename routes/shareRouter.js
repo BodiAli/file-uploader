d2772069207e7f67b16dbef4e3f229fb2929db97
Bodi Ali <bodiali59@gmail.com>
2025-02-08 23:41:11 +0200

    Handle sharing a folder form submit

--- routes/shareRouter.js
+++ routes/shareRouter.js
@@ -4,5 +4,6 @@ const shareController = require("../controllers/shareController");
 const shareRouter = Router();
 
 shareRouter.get("/", shareController.getSharePage);
+shareRouter.post("/create", shareController.createSharedFolder);
 
 module.exports = shareRouter;
