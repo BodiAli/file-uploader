dc62d183294b35c04ccad7d090d52b9c86db9726
Bodi Ali <bodiali59@gmail.com>
2025-02-08 22:17:27 +0200

    Get folders where shared is true

--- controllers/shareController.js
+++ controllers/shareController.js
@@ -1,5 +1,18 @@
 const asyncHandler = require("express-async-handler");
+const prisma = require("../prisma/prismaClient");
 
 exports.getSharePage = asyncHandler(async (req, res) => {
-  res.render("share");
+  const folders = await prisma.folder.findMany({
+    where: {
+      shared: true,
+    },
+    omit: {
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+
+  console.log(folders);
+
+  res.render("share", { folders });
 });
