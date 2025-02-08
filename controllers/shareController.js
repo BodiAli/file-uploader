81e9b0940ff540a8bb1d8e3931cfeedaccb5ecab
Bodi Ali <bodiali59@gmail.com>
2025-02-09 00:36:41 +0200

    Handle creating expiring date and token for shared folders && handle viewing shared folder while validating token and expires date

--- controllers/shareController.js
+++ controllers/shareController.js
@@ -1,5 +1,8 @@
 const asyncHandler = require("express-async-handler");
+const { v4: uuidv4 } = require("uuid");
 const prisma = require("../prisma/prismaClient");
+const NotFoundError = require("../errors/customNotFoundError");
+const GoneError = require("../errors/customGoneError");
 
 exports.getSharePage = asyncHandler(async (req, res) => {
   const sharedFolders = await prisma.folder.findMany({
@@ -16,16 +19,48 @@ exports.getSharePage = asyncHandler(async (req, res) => {
     where: {
       userId: req.user.id,
     },
+    orderBy: {
+      name: "asc",
+    },
   });
-
   console.log("shared folders", sharedFolders);
 
   res.render("share", { sharedFolders, folders });
 });
 
+exports.getSharedFolder = asyncHandler(async (req, res) => {
+  const { token } = req.params;
+
+  const sharedFolder = await prisma.folder.findUnique({
+    where: {
+      shareToken: token,
+    },
+    include: {
+      files: true,
+    },
+  });
+
+  if (!sharedFolder) {
+    throw new NotFoundError("Invalid share link");
+  }
+
+  if (sharedFolder.expiresAt < new Date()) {
+    throw new GoneError("This shared link has expired.");
+  }
+
+  console.log(sharedFolder);
+
+  res.render("shared-folder", { sharedFolder });
+});
+
 exports.createSharedFolder = [
   asyncHandler(async (req, res) => {
-    const { folders: foldersId } = req.body;
+    const { folders: foldersId, duration } = req.body;
+
+    const durationHours = Number(duration);
+    const durationMs = durationHours * 60 * 60 * 1000;
+    const expiresAt = new Date(Date.now() + durationMs);
+
     console.log("foldersId body", foldersId);
 
     if (Array.isArray(foldersId)) {
@@ -36,6 +71,8 @@ exports.createSharedFolder = [
           },
           data: {
             shared: true,
+            shareToken: uuidv4(),
+            shareExpires: expiresAt,
           },
         });
       });
@@ -46,6 +83,8 @@ exports.createSharedFolder = [
         },
         data: {
           shared: true,
+          shareToken: uuidv4(),
+          shareExpires: expiresAt,
         },
       });
     }
