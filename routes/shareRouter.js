abbf1f36f0b726f7e8fc35d8127fd17c9ec21fa5
Bodi Ali <bodiali59@gmail.com>
2025-02-09 00:37:19 +0200

    Handle getting shared folders with the unique token

--- routes/shareRouter.js
+++ routes/shareRouter.js
@@ -4,6 +4,8 @@ const shareController = require("../controllers/shareController");
 const shareRouter = Router();
 
 shareRouter.get("/", shareController.getSharePage);
+shareRouter.get("/:token", shareController.getSharedFolder);
+
 shareRouter.post("/create", shareController.createSharedFolder);
 
 module.exports = shareRouter;
