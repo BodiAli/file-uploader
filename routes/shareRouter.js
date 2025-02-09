3bcf1a36d33a5ce4dfafe93b2a26ff455e0a38fe
Bodi Ali <bodiali59@gmail.com>
2025-02-09 02:24:47 +0200

    Handle requests to download shared file

--- routes/shareRouter.js
+++ routes/shareRouter.js
@@ -5,6 +5,7 @@ const shareRouter = Router();
 
 shareRouter.get("/", shareController.getSharePage);
 shareRouter.get("/:token", shareController.getSharedFolder);
+shareRouter.get("/file/:id", shareController.downloadFile);
 
 shareRouter.post("/create", shareController.createSharedFolder);
 
